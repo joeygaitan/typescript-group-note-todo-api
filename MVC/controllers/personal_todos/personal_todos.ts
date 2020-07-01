940cae4291d2f3f2dd208516f4d16b79b7965e59
joeygaitan <joeygaitan707@gmail.com>
2020-07-01 03:42:23 -0700

    added in personal_todos routes

---
+++ MVC/controllers/personal_todos/personal_todos.ts
@@ -0,0 +1,93 @@
+const personalTodosModels = require('../../models/personal_todos/personal_todos') 
+
+function getAllPersonalTodos (req:any,res:any,next:any) {
+
+    let id = req.claim.id
+    
+    if (!id) throw {status:401, message:"Unauthorized Please login"}
+
+    return personalTodosModels.getAllPersonalTodos(id)
+    .then((data:any)=>{
+        console.log(data)
+        res.status(200).send(data)
+    })
+    .catch((err:any)=>{
+        console.log(err, "inside of the catch of the getAll")
+    })
+}
+
+function getPersonalTodo (req:any,res:any,next:any) {
+
+    let id = req.claim.id
+
+    if (!id) throw {status:401, message:"Unauthorized Please login"}
+
+    return personalTodosModels.getPersonalTodo(id,req.params.id)
+    .then((data:any)=>{
+        if (!data) throw {status: 400, message:"data not found"}
+        res.status(200).send(data)
+    })
+    .catch((err:any)=>{
+        console.log(err,"inside of the catch of the getOne for the personal todo tasks")
+    })
+}
+
+function addPersonalTodo (req:any,res:any,next:any) {
+
+    let id = req.claim.id
+
+    if (!id) throw {status:401, message:"Unauthorized Please login"}
+
+    return personalTodosModels.addPersonalTodo(id, req.body)
+    .then((data:any)=>{
+        if (!data) throw {status: 400, message:"data not found"}
+
+        res.status(201).send(data)
+    })
+    .catch((err:any)=>{
+        console.log(err,"post failed. request body failed. Fields required are active(boolean),container_index(number),container_item_index(number),private(boolean)")
+    })
+}
+
+
+function updatePersonalTodo (req:any,res:any,next:any) {
+
+    let id = req.claim.id
+
+    if (!id) throw {status:401, message:"Unauthorized Please login"}
+
+    return personalTodosModels.updatePersonalTodo(id, req.params.id, req.body)
+    .then((data:any)=>{
+        if (!data) throw {status: 400, message:"data not found"}
+
+        res.status(201).send(data)
+        .catch((err:any)=>{
+            console.log(err,"put failed. some field is missing. Fields required are, id in url path, token, and body wise active(boolean),container_index(number),container_item_index(number),private(boolean)")
+        })
+    })
+}
+
+function removePersonalTodo (req:any,res:any,next:any) {
+
+    let id = req.claim.id
+
+    if (!id) throw {status:401, message:"Unauthorized Please login"}
+
+    return personalTodosModels.removePersonalTodo(id, req.params.id)
+    .then((data:any)=>{
+        if (!data) throw {status: 400, message:"data not found"}
+
+        res.status(201).send(data)
+    })
+    .catch((err:any)=>{
+        console.log(err,"delete failed. wrong id given in url path?")
+    })
+}
+
+module.exports = {
+    getAllPersonalTodos,
+    getPersonalTodo,
+    addPersonalTodo,
+    updatePersonalTodo,
+    removePersonalTodo
+}
---
+++ MVC/routes/personal_todos/personal_todos.ts
@@ -0,0 +1,16 @@
+export{};
+const router = require('express').Router()
+const personal_todosController = require('../../controllers/personal_todos/personal_todo')
+const authMiddleWare = require('../../controllers/login')
+
+router.get('/', authMiddleWare.isAuthenticated, personal_todosController.getAllPersonalTodos)
+
+router.get('/:id', authMiddleWare.isAuthenticated, personal_todosController.getPersonalTodo)
+
+router.post('/', authMiddleWare.isAuthenticated, personal_todosController.addPersonalTodo)
+
+router.put('/:id', authMiddleWare.isAuthenticated, personal_todosController.updatePersonalTodo)
+
+router.delete('/:id', authMiddleWare.isAuthenticated, personal_todosController.removePersonalTodo)
+
+module.exports = router;
