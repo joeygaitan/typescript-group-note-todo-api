da15cebaa4a6020c10b852f4c7a395bb56789398
joeygaitan <joeygaitan707@gmail.com>
2020-08-21 17:26:00 -0700

    fixed a bug with the new notes droptableifexists function on the migrations side. Working on some the database diagram

--- db/migrations/20200821055051_notes.ts
+++ db/migrations/20200821055051_notes.ts
@@ -19,6 +19,6 @@ export async function up(knex: Knex): Promise<any> {
 
 
 export async function down(knex: Knex): Promise<any> {
-    return knex.schema.dropTableIfExists('users')
+    return knex.schema.dropTableIfExists('notes')
 }
 
